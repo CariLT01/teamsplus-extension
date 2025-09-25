c712b1b48c39d4aaf855accf23b51d1cb8e76447
= <publicuser0999847695359@gmail.com>
2025-09-24 20:19:03 -0400

    update extension build

--- scripts/extensionBuild.js
+++ scripts/extensionBuild.js
@@ -1,17 +1,19 @@
-
-
-const childProcess = require("child_process")
+const childProcess = require("child_process");
 const fs = require('fs');
 const archiver = require('archiver');
 
 const pkg = JSON.parse(fs.readFileSync('package.json'));
 
+// Optional command-line argument for version
+// Usage: node buildZip.js 1.2.3
+const versionArg = process.argv[2] || pkg.version;
+
 childProcess.execSync("node scripts/updateManifestJSON");
 
-const output = fs.createWriteStream(`teamsPlus versions/teams_plus@${pkg.version}.zip`);
+const output = fs.createWriteStream(`teamsPlus versions/teams_plus@${versionArg}.zip`);
 const archive = archiver('zip', { zlib: { level: 9 } });
 
-output.on('close', () => console.log(`Created zip: teams_plus@${pkg.version}.zip`));
+output.on('close', () => console.log(`Created zip: teams_plus@${versionArg}.zip`));
 
 archive.pipe(output);
 archive.directory('teams_plus/', false); // replace with your folder
